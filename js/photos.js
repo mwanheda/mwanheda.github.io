6b9b9be7e0382cd99c04a2f84c803f01f6846648
Glascode <mael.querre@gmail.com>
2018-09-22 02:50:50 +0200

    Add image previews

--- js/photos.js
+++ js/photos.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     $('.pictures-container').flexImages({rowHeight: 500});
+
+    /* Display the modal when the user clicks on an item (image) */
+    $('.item').on('click', function () {
+        $('#modal').css('display', 'block');
+        $('.modal-image').attr('src', $(this).find('img').attr('src'));
+        $('#modal-caption').text($(this).find('img').attr('alt'));
+    });
+
+    /* Close the modal when the user clicks on the close button (x)*/
+    $('#modal-close').on('click', function () {
+        $('#modal').css('display', 'none');
+    });
 });
